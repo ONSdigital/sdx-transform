0a19e0fadc59219cac44ec0c94b7ac7f3e75d6eb
TomHolroyd <tomholroyd96@gmail.com>
2021-06-07 12:33:51 +0100

    1 worker

--- run.py
+++ run.py
@@ -8,4 +8,4 @@ logger = structlog.get_logger()
 if __name__ == '__main__':
     logger.info(logger_status)
     logger.info('Starting SDX Transform-cs')
-    os.system("gunicorn transform:app -b :5000 -w 4 -k uvicorn.workers.UvicornWorker")
+    os.system("gunicorn transform:app -b :5000 -w 1 -k uvicorn.workers.UvicornWorker")
