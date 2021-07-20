93d02dd6f457745174e611ad63ddb6709ff193f2
Jon Kent <jonkent95@gmail.com>
2021-07-20 11:30:37 +0100

    Error filter

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import sys
+
 import structlog
 from transform import app
 from gunicorn.app.base import BaseApplication
@@ -22,8 +24,29 @@ class Server(BaseApplication):
         return self.application
 
 
+class ErrorFilter:
+
+    def __init__(self):
+        self.stderr = sys.stderr
+
+    def __getattr__(self, attr_name):
+        return getattr(self.stderr, attr_name)
+
+    def write(self, data):
+        if ' [INFO] ' not in data:
+            self.stderr.write(data)
+            self.stderr.flush()
+        else:
+            sys.stdout.write(data)
+            sys.stdout.flush()
+
+    def flush(self):
+        self.stderr.flush()
+
+
 if __name__ == '__main__':
-    logger.info('Starting SDX Transform-cs')
+    logger.info('Starting SDX Transform')
+    sys.stderr = ErrorFilter()
     options = {
         'bind': '%s:%s' % ('0.0.0.0', '5000'),
         'workers': 2,
