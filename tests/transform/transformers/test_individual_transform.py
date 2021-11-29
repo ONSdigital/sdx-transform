d28d05528cf2936819e6228c24175e403f662e62
Jon Kent <jonkent95@gmail.com>
2021-11-29 15:51:13 +0000

    Test individual transform

---
+++ tests/transform/transformers/test_individual_transform.py
@@ -0,0 +1,42 @@
+import json
+import unittest
+
+from transform.transformers.transform_selector import get_transformer
+
+
+def get_file_as_string(filename):
+    f = open(filename)
+    contents = f.read()
+    f.close()
+    return contents
+
+
+def get_file_as_dict(filename):
+
+    with open(filename, encoding="utf-8") as fh:
+        content = fh.read()
+        return json.loads(content)
+
+
+class TestIndividualSurveyTransformer(unittest.TestCase):
+
+    def test_transform_pck(self):
+        pass
+
+        filename = "./tests/pck/common_software/202.1802.json"
+        expected_filename = "./tests/pck/common_software/202.1802.nobatch"
+
+        submission_dict = get_file_as_dict(filename)
+        expected = get_file_as_string(expected_filename)
+        print("Expected:")
+        print(expected)
+
+        transformer = get_transformer(submission_dict)
+        pck_name, pck = transformer.create_pck()
+
+        actual = pck
+        print(" ")
+        print("Actual:")
+        print(actual)
+
+        self.assertEqual(expected, actual)
