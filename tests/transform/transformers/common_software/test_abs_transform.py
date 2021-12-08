7052c76d0bcd1f4003ab5a6242505cb7292f007f
Jon Kent <jonkent95@gmail.com>
2021-12-08 11:08:07 +0000

    ABS to name pck files by sector id (#15)

--- tests/transform/transformers/common_software/test_abs_transform.py
+++ tests/transform/transformers/common_software/test_abs_transform.py
@@ -1,5 +1,7 @@
+import json
 import unittest
 
+from transform.transformers.common_software.abs_transformer import ABSTransformer
 from transform.transformers.common_software.cs_formatter import CSFormatter
 
 
@@ -11,3 +13,12 @@ class ABSTests(unittest.TestCase):
         """
         return_value = CSFormatter._idbr_receipt("202", "12346789012", "A", "21")
         self.assertEqual("12346789012:A:202:202112", return_value)
+
+    def test_pck_name(self):
+        with open("tests/replies/202.1802.json", "r") as fp:
+            response = json.load(fp)
+
+        response['tx_id'] = "11111111-2222-3333-4444-555555555555"
+        print(response)
+        pck_name, pck = ABSTransformer(response).create_pck()
+        self.assertEqual("053_1111111122223333", pck_name)
