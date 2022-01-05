3f50342a8f3c99b883adb168e3134d437678d21b
Luke Joseph <ldjdev12@gmail.com>
2022-01-05 11:42:32 +0000

    Change uwsgi from gUnicorn to Waitress

--- run.py
+++ run.py
@@ -2,26 +2,27 @@ import sys
 
 import structlog
 from transform import app
-from gunicorn.app.base import BaseApplication
+from waitress import serve
+
 
 logger = structlog.get_logger()
 
 
-class Server(BaseApplication):
-
-    def __init__(self, application, options_dict=None):
-        self.options = options_dict or {}
-        self.application = application
-        super().__init__()
-
-    def load_config(self):
-        config = {key: value for key, value in self.options.items()
-                  if key in self.cfg.settings and value is not None}
-        for key, value in config.items():
-            self.cfg.set(key.lower(), value)
-
-    def load(self):
-        return self.application
+# class Server(BaseApplication):
+#
+#     def __init__(self, application, options_dict=None):
+#         self.options = options_dict or {}
+#         self.application = application
+#         super().__init__()
+#
+#     def load_config(self):
+#         config = {key: value for key, value in self.options.items()
+#                   if key in self.cfg.settings and value is not None}
+#         for key, value in config.items():
+#             self.cfg.set(key.lower(), value)
+#
+#     def load(self):
+#         return self.application
 
 
 class ErrorFilter:
@@ -47,8 +48,4 @@ class ErrorFilter:
 if __name__ == '__main__':
     logger.info('Starting SDX Transform')
     sys.stderr = ErrorFilter()
-    options = {
-        'bind': '%s:%s' % ('0.0.0.0', '5000'),
-        'workers': 2,
-    }
-    Server(app, options).run()
+    serve(app, host='0.0.0.0', port=5000)
