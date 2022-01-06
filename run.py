f72138ebd8484f7441f080fca7bfb875b7ba65ef
Luke Joseph <ldjdev12@gmail.com>
2022-01-06 14:58:42 +0000

    update dependencies and refactor

--- run.py
+++ run.py
@@ -1,28 +1,10 @@
 import sys
-
 import structlog
+
 from transform import app
 from waitress import serve
 
-
 logger = structlog.get_logger()
-
-
-# class Server(BaseApplication):
-#
-#     def __init__(self, application, options_dict=None):
-#         self.options = options_dict or {}
-#         self.application = application
-#         super().__init__()
-#
-#     def load_config(self):
-#         config = {key: value for key, value in self.options.items()
-#                   if key in self.cfg.settings and value is not None}
-#         for key, value in config.items():
-#             self.cfg.set(key.lower(), value)
-#
-#     def load(self):
-#         return self.application
 
 
 class ErrorFilter:
