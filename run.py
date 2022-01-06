ebee359da1b71ec0b8044f39b3d7620ec95c4786
Luke Joseph <ldjdev12@gmail.com>
2022-01-06 15:20:57 +0000

    refactor

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-import sys
 import structlog
 
 from transform import app
@@ -7,27 +6,6 @@ from waitress import serve
 logger = structlog.get_logger()
 
 
-class ErrorFilter:
-
-    def __init__(self):
-        self.stderr = sys.stderr
-
-    def __getattr__(self, attr_name):
-        return getattr(self.stderr, attr_name)
-
-    def write(self, data):
-        if ' [INFO] ' not in data:
-            self.stderr.write(data)
-            self.stderr.flush()
-        else:
-            sys.stdout.write(data)
-            sys.stdout.flush()
-
-    def flush(self):
-        self.stderr.flush()
-
-
 if __name__ == '__main__':
     logger.info('Starting SDX Transform')
-    sys.stderr = ErrorFilter()
     serve(app, host='0.0.0.0', port=5000)
