a50386b4a8f60198c26ff34999f14c3a60a9a80e
Jon Kent <jonkent95@gmail.com>
2022-02-02 10:28:52 +0000

    transform map

---
+++ transform/transformers/cord/des/des_transformer.py
@@ -0,0 +1,95 @@
+import decimal
+from decimal import Decimal, ROUND_HALF_UP
+
+import structlog
+
+from transform.transformers.cord.cord_formatter import CORDFormatter
+from transform.transformers.cord.des.des_transforms import transformations
+from transform.transformers.survey_transformer import SurveyTransformer
+
+logger = structlog.get_logger()
+
+
+def round_and_divide_by_one_thousand(value):
+    """Rounding is done on a ROUND_HALF_UP basis and values are divided by 1000 for the pck"""
+    try:
+        # Set the rounding context for Decimal objects to ROUND_HALF_UP
+        decimal.getcontext().rounding = ROUND_HALF_UP
+        return Decimal(round(Decimal(float(value))) / 1000).quantize(1)
+
+    except TypeError:
+        logger.info("Tried to quantize a NoneType object. Returning an empty string")
+        return ''
+
+
+class DESTransformer(SurveyTransformer):
+    """Perform the transforms and formatting for the DES survey."""
+
+    def __init__(self, response, seq_nr=0):
+        super().__init__(response, seq_nr)
+        period = response['collection']['period']
+        if len(period) == 4:
+            response['collection']['period'] = period[2:] + '12'
+
+        self.period = period
+
+    def _get_value(self, q_code):
+        input_dict = self.response['data']
+        if q_code in input_dict:
+            value = input_dict.get(q_code)
+            return value if value != '' else None
+        else:
+            return None
+
+    def transform(self):
+        result = {}
+
+        for q_code, transformation in transformations.items():
+
+            value = self._get_value(q_code)
+            transformed_value = value
+
+            if value is None:
+                if type(transformation) == dict:
+                    transformed_value = transformation.get(value)
+
+                elif transformation == 'comment':
+                    transformed_value = 2
+
+                else:
+                    continue
+
+            elif type(transformation) == dict:
+                transformed_value = transformation.get(value)
+            elif transformation == 'nearest_thousand':
+                transformed_value = round_and_divide_by_one_thousand(value)
+            elif transformation == 'value':
+                transformed_value = value
+            elif transformation == 'comment':
+                transformed_value = 1 if value != "" else 2
+
+            result[q_code] = transformed_value
+
+        return result
+
+    def _create_pck(self, transformed_data):
+        """Return a pck file using provided data"""
+        pck = CORDFormatter.get_pck(
+            transformed_data,
+            self.ids.survey_id,
+            self.ids.ru_ref,
+            self.period,
+        )
+        return pck
+
+    def create_pck(self):
+        bound_logger = logger.bind(ru_ref=self.ids.ru_ref, tx_id=self.ids.tx_id)
+        bound_logger.info("Transforming data for processing")
+        transformed_data = self.transform()
+        bound_logger.info("Data successfully transformed")
+
+        bound_logger.info("Creating PCK")
+        pck_name = CORDFormatter.pck_name(self.ids.survey_id, self.ids.tx_id)
+        pck = self._create_pck(transformed_data)
+        bound_logger.info("Successfully created PCK")
+        return pck_name, pck
---
+++ transform/transformers/cord/des/des_transforms.py
@@ -0,0 +1,650 @@
+
+
+# The following dictionary defines the transformations to perform.
+# The key is the qcode, the value describes what transformation needs to be done on the value.
+# A dict for the value generally describes what to do with a radio button input and a
+# list indicates a function to call (at index 0) and the arguments to pass to that function.
+
+transformations = {
+    "22": "value",
+    "356": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "277": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "452": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "600": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "601": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "602": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "603": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "604": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "605": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "607": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "608": "nearest_thousand",
+    "609": "nearest_thousand",
+    "610": "nearest_thousand",
+    "611": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "612": "nearest_thousand",
+    "613": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "614": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "615": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "616": "nearest_thousand",
+    "617": "nearest_thousand",
+    "618": "nearest_thousand",
+    "619": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "620": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "621": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "622": "nearest_thousand",
+    "623": "nearest_thousand",
+    "624": "nearest_thousand",
+    "625": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "626": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "627": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "628": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "629": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "630": "nearest_thousand",
+    "631": "nearest_thousand",
+    "632": "nearest_thousand",
+    "633": "nearest_thousand",
+    "634": "nearest_thousand",
+    "635": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "636": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "637": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "638": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "639": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "640": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "641": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "642": "nearest_thousand",
+    "643": "nearest_thousand",
+    "644": "nearest_thousand",
+    "645": "nearest_thousand",
+    "646": "nearest_thousand",
+    "647": "nearest_thousand",
+    "648": "nearest_thousand",
+    "649": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "650": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "651": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "652": "nearest_thousand",
+    "653": "nearest_thousand",
+    "654": "nearest_thousand",
+    "655": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "656": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "657": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "658": "nearest_thousand",
+    "659": "nearest_thousand",
+    "660": "nearest_thousand",
+    "661": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "662": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "663": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "664": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "665": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "666": "nearest_thousand",
+    "667": "nearest_thousand",
+    "668": "nearest_thousand",
+    "669": "nearest_thousand",
+    "670": "nearest_thousand",
+    "671": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "672": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "673": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "674": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "675": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "676": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "677": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "678": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "679": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "680": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "681": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "682": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "683": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "684": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "685": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "686": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "687": "nearest_thousand",
+    "688": "nearest_thousand",
+    "689": "nearest_thousand",
+    "690": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "691": "nearest_thousand",
+    "692": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "693": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "694": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "695": "nearest_thousand",
+    "696": "nearest_thousand",
+    "697": "nearest_thousand",
+    "698": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "699": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "700": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "701": "nearest_thousand",
+    "702": "nearest_thousand",
+    "703": "nearest_thousand",
+    "704": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "705": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "706": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "707": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "708": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "709": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "710": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "711": "nearest_thousand",
+    "712": "nearest_thousand",
+    "713": "nearest_thousand",
+    "714": "nearest_thousand",
+    "715": "nearest_thousand",
+    "716": "nearest_thousand",
+    "717": "nearest_thousand",
+    "718": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "719": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "720": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "721": "nearest_thousand",
+    "722": "nearest_thousand",
+    "723": "nearest_thousand",
+    "724": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "725": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "726": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "727": "nearest_thousand",
+    "728": "nearest_thousand",
+    "729": "nearest_thousand",
+    "730": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "731": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "732": "nearest_thousand",
+    "733": "nearest_thousand",
+    "734": "nearest_thousand",
+    "736": "nearest_thousand",
+    "738": "nearest_thousand",
+    "739": "nearest_thousand",
+    "740": "nearest_thousand",
+    "741": "nearest_thousand",
+    "742": "nearest_thousand",
+    "743": "nearest_thousand",
+    "744": "nearest_thousand",
+    "745": "nearest_thousand",
+    "746": "nearest_thousand",
+    "747": "nearest_thousand",
+    "748": "nearest_thousand",
+    "749": "nearest_thousand",
+    "750": "nearest_thousand",
+    "751": "nearest_thousand",
+    "752": "nearest_thousand",
+    "753": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "754": "nearest_thousand",
+    "755": "nearest_thousand",
+    "756": "nearest_thousand",
+    "757": "nearest_thousand",
+    "758": "nearest_thousand",
+    "759": "nearest_thousand",
+    "760": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "761": "\u00a3's",
+    "762": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "763": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "190": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "764": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "765": "nearest_thousand",
+    "766": "nearest_thousand",
+    "767": "nearest_thousand",
+    "768": "nearest_thousand",
+    "769": "nearest_thousand",
+    "365": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "364": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "359": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "363": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "361": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "360": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "362": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "770": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "771": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "431": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "432": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "433": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "434": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "772": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "515": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "516": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "517": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "773": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "774": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "775": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "776": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "777": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "778": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "780": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "781": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "782": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "783": {
+        "ticked": "1",
+        "unticked": "0"
+    },
+    "518": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "495": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "784": "nearest_thousand",
+    "785": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "786": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "787": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "788": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "789": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "790": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "791": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "792": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "793": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "794": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "795": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "796": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "797": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "484": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "483": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "486": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "487": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "481": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "275": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "274": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "272": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "482": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "485": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "798": {
+        "Yes": "10",
+        "No": "01"
+    },
+    "500": {
+        "Yes": "10",
+        "No": "01"
+    }
+}
