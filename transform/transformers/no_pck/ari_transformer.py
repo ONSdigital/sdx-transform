24250030157716ec91ee7d23701ab23c6c5b4997
Jon Kent <jonkent95@gmail.com>
2022-03-14 15:22:09 +0000

    transformer for railways

--- transform/transformers/no_pck/ari_transformer.py
+++ transform/transformers/no_pck/ari_transformer.py
@@ -3,9 +3,9 @@ import json
 from transform.transformers.survey_transformer import SurveyTransformer
 
 
-class QFITransformer(SurveyTransformer):
-    """Transformer for the Quarterly Fuels Investment Survey.
-    QFI does not require a pck file but does need a bespoke name for the json file."""
+class ARITransformer(SurveyTransformer):
+    """Transformer for the Annual Railways Investment Survey.
+    ARI does not require a pck file but does need a bespoke name for the json file."""
 
     def get_json(self):
         json_name = f"{self.ids.survey_id}_{self.ids.ru_ref}_{self.ids.period}.json"
