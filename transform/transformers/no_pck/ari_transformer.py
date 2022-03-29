be5cd86cc00791152085a67ce8469a3760a6d62f
Jon Kent <jonkent95@gmail.com>
2022-03-29 09:48:00 +0100

    Update comment

--- transform/transformers/no_pck/ari_transformer.py
+++ transform/transformers/no_pck/ari_transformer.py
@@ -2,10 +2,35 @@ import json
 
 from transform.transformers.survey_transformer import SurveyTransformer
 
+qcode_mapping = {
+    "2": "1.1",
+    "3": "1.2",
+    "4": "2.1",
+    "5": "2.2",
+    "6": "3.1",
+    "7": "3.2",
+    "8": "3.3",
+    "9": "3.4",
+    "10": "4.1",
+    "11": "4.2",
+    "146": "146"
+}
+
 
 class ARITransformer(SurveyTransformer):
-    """Transformer for the Annual Railways Investment Survey.
-    ARI does not require a pck file but does need a bespoke name for the json file."""
+    """
+    Transformer for the Annual Railways Investment Survey.
+    ARI does not require a pck file but does need a bespoke name for the json file.
+
+    The json received from EQ contains qcodes, however there is a requirement for
+    the images and json to use the actual question id (short code) instead.
+    These are therefore mapped in before any processing takes place.
+    """
+
+    def __init__(self, response, seq_nr=0):
+        data = {qcode_mapping.get(k, k): v for k, v in response['data'].items()}
+        response['data'] = data
+        super().__init__(response, seq_nr)
 
     def get_json(self):
         json_name = f"{self.ids.survey_id}_{self.ids.ru_ref}_{self.ids.period}.json"
