fec2d9824c88f0c7625da1c5dd56ecdd1ea2582d
Angus Goody <angus.goody@gmail.com>
2023-02-14 11:08:52 +0000

    Added google cloud secret manager & extract ftp path

--- transform/settings.py
+++ transform/settings.py
@@ -2,6 +2,8 @@ import os
 
 import structlog
 
+from transform.secret_manager import get_secret
+
 logger = structlog.get_logger()
 
 
@@ -15,7 +17,9 @@ def _get_value(key, default_value=None):
     return value
 
 
-FTP_PATH = _get_value("FTP_PATH", "\\")
+PROJECT_ID = os.getenv('PROJECT_ID')
+
+FTP_PATH = get_secret(PROJECT_ID, "FTP_PATH")
 SDX_FTP_IMAGE_PATH = _get_value("SDX_FTP_IMAGES_PATH", "EDC_QImages")
 
 SDX_FTP_DATA_PATH = "EDC_QData"
