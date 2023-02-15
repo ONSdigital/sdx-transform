2a34d1a0010939e56a7ec4363374f7d93040e6e2
Jon Kent <jonkent95@gmail.com>
2023-02-15 16:55:23 +0000

    Read secret in cloud config

--- run.py
+++ run.py
@@ -3,9 +3,12 @@ import structlog
 from transform import app
 from waitress import serve
 
+from transform.settings import cloud_config
+
 logger = structlog.get_logger()
 
 
 if __name__ == '__main__':
     logger.info('Starting SDX Transform')
+    cloud_config()
     serve(app, host='0.0.0.0', port=5000)
