0a037f72c426779eb41cc99312e6ca5001909ba0
ldearsley <ldearsley2@gmail.com>
2023-02-27 15:05:04 +0000

    berd testing

--- tests/transform/transformers/spp/test_berd_transformer.py
+++ tests/transform/transformers/spp/test_berd_transformer.py
@@ -1,6 +1,7 @@
+import json
 import unittest
 
-from transform.transformers.spp.berd.berd_transformer import Answer
+from transform.transformers.spp.berd.berd_transformer import Answer, extract_answers, convert_to_spp, SPP
 
 
 class BerdTransformerTests(unittest.TestCase):
@@ -14,7 +15,7 @@ class BerdTransformerTests(unittest.TestCase):
             "answers": [
                 {
                     "answer_id": "answerb3840e82-aeaf-4a85-8556-84f2e1e5388a",
-                    "value": "Yes, I can report for this period"
+                    "value": "Yes"
                 },
             ],
             "lists": [],
@@ -26,6 +27,141 @@ class BerdTransformerTests(unittest.TestCase):
             ]
         }
 
-        answer = Answer()
-        answer.qcode = 1
-        pass
+        actual = extract_answers(data)
+        expected = [Answer("101", "Yes", None, None)]
+
+        self.assertEqual(actual, expected)
+
+    def test_get_multiple_qcodes_from_json(self):
+        data = {
+            "answers": [
+                {
+                    "answer_id": "answerb3840e82-aeaf-4a85-8556-84f2e1e5388a",
+                    "value": "Yes"
+                },
+                {
+                    "answer_id": "answerb3840e82-aeaf-4a85-8556-84f2e1e5388b",
+                    "value": "No"
+                }
+            ],
+            "lists": [],
+            "answer_codes": [
+                {
+                    "answer_id": "answerb3840e82-aeaf-4a85-8556-84f2e1e5388b",
+                    "code": "101"
+                },
+                {
+                    "answer_id": "answerb3840e82-aeaf-4a85-8556-84f2e1e5388a",
+                    "code": "102"
+                }
+            ]
+        }
+
+        actual = extract_answers(data)
+        expected = [Answer("102", "Yes", None, None), Answer("101", "No", None, None)]
+
+        self.assertEqual(actual, expected)
+
+
+    def test_add_list_item_id_to_answer(self):
+        data = {
+            "answers": [
+                {
+                    "answer_id": "answerb3840e82-aeaf-4a85-8556-84f2e1e5388a",
+                    "value": "Yes",
+                    "list_item_id": "YxAbgY"
+                },
+                {
+                    "answer_id": "answerb3840e82-aeaf-4a85-8556-84f2e1e5388b",
+                    "value": "No",
+                    "list_item_id": "IBzcQr"
+                }
+            ],
+            "lists": [
+                {"items": ["YxAbgY", "IBzcQr"], "name": "product_codes"}
+            ],
+            "answer_codes": [
+                {
+                    "answer_id": "answerb3840e82-aeaf-4a85-8556-84f2e1e5388b",
+                    "code": "101"
+                },
+                {
+                    "answer_id": "answerb3840e82-aeaf-4a85-8556-84f2e1e5388a",
+                    "code": "102"
+                }
+            ]
+        }
+
+        actual = extract_answers(data)
+        expected = [Answer("102", "Yes", "YxAbgY", "product_codes"), Answer("101", "No", "IBzcQr", "product_codes")]
+
+        self.assertEqual(actual, expected)
+
+    def test_convert_to_spp(self):
+        answer_list = [Answer("102", "Yes", "YxAbgY", "product_codes"), Answer("101", "No", "IBzcQr", "product_codes")]
+        actual = convert_to_spp(answer_list)
+        expected = [SPP("102", "Yes", 1), SPP("101", "No", 1)]
+
+        self.assertEqual(actual, expected)
+
+    def test_convert_to_spp_with_multiple_instances(self):
+        answer_list = [Answer("101", "No", "123", "product_codes"), Answer("102", "Yes", "321", "product_codes")]
+        actual = convert_to_spp(answer_list)
+        expected = [SPP("101", "No", 1), SPP("102", "Yes", 2)]
+
+        self.assertEqual(actual, expected)
+
+    def test_convert_to_spp_with_multiple_groups(self):
+        answer_list = [
+            Answer("101", "No", "123", "product_codes"),
+            Answer("102", "Yes", "321", "product_codes"),
+            Answer("103", "Yes", "321", "product_codes_2")
+        ]
+        actual = convert_to_spp(answer_list)
+        expected = [SPP("101", "No", 1), SPP("102", "Yes", 2), SPP("103", "Yes", 1)]
+
+        self.assertEqual(actual, expected)
+
+    def test_convert_to_spp_with_multiple_groups_2(self):
+        answer_list = [
+            Answer("100", "Yes", None, None),
+            Answer("101", "No", None, None),
+            Answer("102", "No", "123", "group_1"),
+            Answer("102", "Yes", "321", "group_1"),
+            Answer("102", "No", "144", "group_1"),
+            Answer("256", "Yes", "999", "group_2"),
+            Answer("257", "No", "999", "group_2"),
+            Answer("389", "Yes", "321", "group_3"),
+            Answer("303", "Yes", "444", "group_3"),
+            Answer("309", "Yes", "444", "group_3"),
+            Answer("303", "Yes", "555", "group_3"),
+
+        ]
+        actual = convert_to_spp(answer_list)
+        expected = [
+            SPP("100", "Yes", 0),
+            SPP("101", "No", 0),
+            SPP("102", "No", 1),
+            SPP("102", "Yes", 2),
+            SPP("102", "No", 3),
+            SPP("256", "Yes", 1),
+            SPP("257", "No", 1),
+            SPP("389", "Yes", 1),
+            SPP("303", "Yes", 2),
+            SPP("309", "Yes", 2),
+            SPP("303", "Yes", 3),
+
+
+        ]
+
+        self.assertEqual(actual, expected)
+
+    # def test_example_berd(self):
+    #     f = open("transform/transformers/spp/berd/berd_example.json")
+    #     result: list = json.load(f)
+    #
+    #     data = result["data"]
+    #     extracted = extract_answers(data)
+    #
+    #     for line in extracted:
+    #         print(line)
--- transform/transformers/spp/berd/berd_transformer.py
+++ transform/transformers/spp/berd/berd_transformer.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Union
+from typing import Union, Dict, List
 
 
 @dataclass(order=True)
@@ -7,5 +7,62 @@ class Answer:
     qcode: str
     value: Union[str, None]
     list_item_id: Union[str, None]
+    group: Union[str, None]
+
+
+@dataclass(order=True)
+class SPP:
+    question_code: str
+    response: Union[str, None]
+    instance: int
+
+
+def extract_answers(data: Dict) -> List[Answer]:
+
+    answer_list = []
+
+    for x in data["answers"]:
+        answer_id = x["answer_id"]
+        list_item_id = x.get("list_item_id")
+        value = x["value"]
+        qcode, group = None, None
+
+        for y in data["answer_codes"]:
+            if y["answer_id"] == answer_id:
+                qcode = y["code"]
+
+        for z in data["lists"]:
+            if list_item_id in z["items"]:
+                group = z["name"]
+
+        answer_list.append(Answer(qcode, value, list_item_id, group))
+
+    return answer_list
+
+
+def convert_to_spp(answer_list: List[Answer]) -> List[SPP]:
+
+    spp_list = []
+    group_dict = {}
+
+    for answer in answer_list:
+        instance = 0
+        if answer.group:
+            if answer.group not in group_dict:
+                group_dict[answer.group] = []
+
+        if answer.list_item_id:
+            instance_list = group_dict[answer.group]
+            if answer.list_item_id not in instance_list:
+                instance_list.append(answer.list_item_id)
+            instance = instance_list.index(answer.list_item_id) + 1
+
+        spp = SPP(answer.qcode, answer.value, instance)
+        spp_list.append(spp)
+
+    return spp_list
+
+
+
 
 
