8510f637b13f1266893c52482c2e8cbb273a0050
Lucas Dearsley <93087356+ldearsley2@users.noreply.github.com>
2023-06-05 15:09:25 +0100

    Add credit grantors transformer (#72)
    * Add credit grantors transformer, add test data, example data tested, added to transform selector
    
    * Remove addition transformations and added negative values to the tests.
    
    * version update
    
    * use sdx logger
    
    * change credit grantors test class name
    
    * change credit grantors test data
    
    * version update

---
+++ transform/transformers/cord/credit_grantors/credit_grantors_transformer.py
@@ -0,0 +1,63 @@
+from typing import Dict
+from sdx_gcp.app import get_logger
+
+from transform.settings import USE_IMAGE_SERVICE
+from transform.transformers.common_software.cs_formatter import CSFormatter
+from transform.transformers.cord.credit_grantors.credit_grantors_transform_spec import Transform, TRANSFORMS_SPEC
+from transform.transformers.response import SurveyResponse
+from transform.transformers.survey_transformer import SurveyTransformer
+
+logger = get_logger()
+
+
+def perform_transforms(data: Dict[str, str], transforms_spec: Dict[str, Transform]) -> Dict[str, int]:
+
+    output_dict = {}
+
+    for k, v in transforms_spec.items():
+        try:
+            if k not in data:
+                continue
+
+            if v == Transform.NO_TRANSFORM:
+                output_dict[k] = int(data[k])
+
+        except ValueError:
+            logger.error(f"Unable to process qcode: {k} as received non numeric value: {v}")
+
+    return output_dict
+
+
+def extract_pck_period(period: str) -> str:
+    if len(period) <= 4:
+        return period
+    return period[2:]
+
+
+class CreditGrantorsTransformer(SurveyTransformer):
+
+    def __init__(self, response: SurveyResponse, seq_nr=0):
+        super().__init__(response, seq_nr, use_sdx_image=USE_IMAGE_SERVICE)
+
+    def _format_pck(self, transformed_data) -> str:
+        """Common software require Blocks to be form type 0001 even though it is 0002 in Author"""
+        pck = CSFormatter.get_pck(
+            transformed_data,
+            "0001",
+            self.survey_response.ru_ref,
+            self.survey_response.ru_check,
+            extract_pck_period(self.survey_response.period),
+        )
+        return pck
+
+    def create_pck(self):
+        bound_logger = logger.bind(ru_ref=self.survey_response.ru_ref, tx_id=self.survey_response.tx_id)
+        bound_logger.info("Transforming data for processing")
+        transformed_data = perform_transforms(self.survey_response.data, TRANSFORMS_SPEC)
+        bound_logger.info("Data successfully transformed")
+
+        bound_logger.info("Creating PCK")
+        pck_name = CSFormatter.pck_name(self.survey_response.survey_id, self.survey_response.tx_id)
+        pck = self._format_pck(transformed_data)
+        bound_logger.info("Successfully created PCK")
+        return pck_name, pck
--- transform/transformers/transform_selector.py
+++ transform/transformers/transform_selector.py
@@ -8,6 +8,7 @@ from transform.transformers.common_software.sand_and_gravel.marine_dredged_trans
 from transform.transformers.cora.mes_transformer import MESTransformer
 from transform.transformers.cora.ukis.ukis_transformer import UKISTransformer
 from transform.transformers.cord import EcommerceTransformer
+from transform.transformers.cord.credit_grantors.credit_grantors_transformer import CreditGrantorsTransformer
 from transform.transformers.cord.des.des2021_transformer import DES2021Transformer
 from transform.transformers.cord.des.des_transformer import DESTransformer
 from transform.transformers.no_pck.ari_transformer import ARITransformer
@@ -46,6 +47,8 @@ def get_transformer(response: SurveyResponse, sequence_no=1000):
                 transformer = DESTransformer(response, sequence_no)
         else:
             transformer = EcommerceTransformer(response, sequence_no)
+    elif survey_id == "127":
+        transformer = CreditGrantorsTransformer(response, sequence_no)
 
     # NO PCK INQUIRIES
     elif survey_id == "007":
