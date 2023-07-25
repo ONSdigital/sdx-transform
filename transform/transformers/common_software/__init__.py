9261148d4f256af1f3c2dfda054efaeeefd5952a
Jon Kent <jonkent95@gmail.com>
2023-07-25 16:43:26 +0100

    fix

--- transform/transformers/common_software/__init__.py
+++ transform/transformers/common_software/__init__.py
@@ -1,6 +1,5 @@
 from .cs_transformer import CSTransformer
 from .mbs_transformer import MBSTransformer
-from .mwss_transformer import MWSSTransformer
 from .pck_transformer import PCKTransformer
 
-__all__ = ['CSTransformer', 'MBSTransformer', 'MWSSTransformer', 'PCKTransformer']
+__all__ = ['CSTransformer', 'MBSTransformer', 'PCKTransformer']
